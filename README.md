98282175342bbae517b61e0aa238ad875e8cd5ba
zhabdykadyrova <zhabdykadyrova@akumoproject.com>
2024-01-10 11:09:54 -0500

    completed all tasks

--- README.md
+++ README.md
@@ -1,23 +1,23 @@
 # Project Information:
 This Python project reads the Spreadsheet file ("inventory.xlsx") and automates the following tasks:
 
-1: Calculate how many Products we have per Supplier
+1: Calculate how many Products we have per Supplier \n
 Result should look like this: {'AAA Company': 43, 'BBB Company': 17, 'CCC Company': 14 }
 
-2: List Products with Inventory less than 10
+2: List each company with respective total Inventory value
 
-3: List each company with respective total Inventory value
+3: List Products with Inventory less than 10
 
-4: Calculate Inventory value for each Product
+4: Add a fifth column to the spreadsheetnfor total inventory price
 
 # Instructions: 
-- Install "openpyxl" package for it to be able to read spreadsheet file: pip install openpyxl
+- Install "openpyxl" package for it to be able to read spreadsheet file: pip install openpyxl \n
 Documentation of the module: https://openpyxl.readthedocs.io/en/stable/api/openpyxl.worksheet.worksheet.html#module-openpyxl.worksheet.worksheet
-
-- Copy the spreadsheet file into your repo and ingest it to python: 
-inv_file = openpyxl.load_workbook("inventory.xlsx")
+- Copy the spreadsheet file into your repo and ingest it to python: \n
+inv_file = openpyxl.load_workbook("inventory.xlsx") \n
 product_list = inv_file["Sheet1"]
-
-Task 1: 
 - In order to calculate how many Products we have per Supplier, we have to utilize for loop to read/count EACH row. 
-- 
+- use range() since it creates a sequence of numbers, starting from 0 by default. Start from row 2 and add +1 to the max_row.
+- Identify where every cell is located, Supplier name for example is on: cell(product_row, 4)
+- Follow the code for the logic
+
--- main.py
+++ main.py
@@ -3,14 +3,67 @@ import openpyxl
 inv_file = openpyxl.load_workbook("inventory.xlsx")
 product_list = inv_file["Sheet1"]
 
-# Task 1:
+# Task 1: Calculate how many Products we have per Supplier
 products_per_supplier = {}
 
 for product_row in range(2, product_list.max_row + 1):
-    supplier_name = product_list.cell(product_row, 4)
+    supplier_name = product_list.cell(product_row, 4).value
 
     if supplier_name in products_per_supplier:
-        current_num_products = products_per_supplier[supplier_name]
+        current_num_products = products_per_supplier[supplier_name] # or you can use products_per_supplier.get(supplier_name)
         products_per_supplier[supplier_name] = current_num_products + 1
     else:
         products_per_supplier[supplier_name] = 1
+
+print(f"Task 1: {products_per_supplier}")
+
+# Task 2: List each company with respective total Inventory value
+total_value_per_supplier = {}
+
+for product_row in range(2, product_list.max_row + 1):
+    supplier_name = product_list.cell(product_row, 4).value
+    inventory = product_list.cell(product_row, 2).value
+    price = product_list.cell(product_row, 3).value
+
+    if supplier_name in total_value_per_supplier:
+        current_total_value = total_value_per_supplier.get(supplier_name)
+        total_value_per_supplier[supplier_name] = current_total_value + inventory * price
+    else: 
+        total_value_per_supplier[supplier_name] = inventory * price
+
+print(f"Task 2: {total_value_per_supplier}")
+
+#Task 3: List Products with Inventory less than 10
+products_under_10_inv = {}
+
+for product_row in range(2, product_list.max_row + 1):
+    supplier_name = product_list.cell(product_row, 4).value
+    inventory = product_list.cell(product_row, 2).value
+    price = product_list.cell(product_row, 3).value
+    product_num = product_list.cell(product_row, 1).value
+
+    if inventory < 10: 
+        products_under_10_inv[int(product_num)] = int(inventory)
+
+print(f"Task 3: {products_under_10_inv}")
+
+#Task 4: Add a fifth column to the spreadsheetnfor total inventory price
+
+for product_row in range(2, product_list.max_row + 1):
+    supplier_name = product_list.cell(product_row, 4).value
+    inventory = product_list.cell(product_row, 2).value
+    price = product_list.cell(product_row, 3).value
+    product_num = product_list.cell(product_row, 1).value
+    inventory_price = product_list.cell(product_row, 5)
+
+    inventory_price.value = inventory * price
+
+inv_file.save("inventory_with_total_value.xlsx")
+
+
+
+
+
+
+
+
